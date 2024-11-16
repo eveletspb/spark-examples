7198a400e546f5656707a59fd5ca6a1a4dc75620
eveletspb <sergey.bolshanin@mytona.com>
2024-11-16 19:26:22 +0700

    completed example simple spark application

--- sprk/src/com.sbol.bigdata/simple/SimpleSpark.scala
+++ sprk/src/com.sbol.bigdata/simple/SimpleSpark.scala
@@ -5,6 +5,8 @@ import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.DataTypes
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SaveMode
 
 object SimpleSpark extends App {
 
@@ -48,4 +50,35 @@ object SimpleSpark extends App {
 
   val resultList = flatMapDs.collect()
 
+  val personsDs: Dataset[Person] = Seq(
+    Person("Alice", 2000, 25, 2024),
+    Person("Tom", 4000, 20, 2025),
+    Person("Jonh", 6000, 17, 2020),
+    Person("Vladimir", 9999, 29, 2099)
+  ).toDS()
+
+  val averageSalary = personsDs
+    .agg(avg("salary").as("avg_salary"))
+    .as[Double]
+    .show()
+
+  val maxAge = personsDs
+    .agg(max("age").as("max_age"))
+    .as[Double]
+    .show()
+
+  val averageAge2 = personsDs
+    .select(
+      avg("salary").as("avg_salary")
+    )
+    .as[Double]
+    .show()
+
+    personsDs.write
+    .mode(SaveMode.Overwrite)
+    .partitionBy("year")
+    .parquet("dataset/parquet")
+
 }
+
+case class Person(name: String, salary: Double, age: Int, year: Int)
